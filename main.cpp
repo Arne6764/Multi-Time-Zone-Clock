5c1b0d099d5cb21f793438a3cc4d997db1c4a580
Arne6764 <156936597+Arne6764@users.noreply.github.com>
2024-06-04 19:29:45 -0400

    Updated main.cpp
    Added a second time zone (UTC)

--- main.cpp
+++ main.cpp
@@ -11,12 +11,16 @@ void EST_Time(int hour, int minute, int second) {
   std::cout << "Here's the time in EST: " << hour << ":" << minute << ":" << second << std::endl;
 }
 
+void UTC_Time(int hour, int minute, int second) {
+  std::cout << "Here's the time in UTC: " << hour << ":" << minute << ":" << second << std::endl;
+}
+
 int main() {
   std::cout << "Multi-Time Zone Clock\n";
-  std::cout << "Choose your time zone:\n 1) EST\n 2) IDK, tell me one to add\n E) Exit\n";
+  std::cout << "Choose your time zone:\n 1) EST\n 2) UTC\n E) Exit\n";
   std::string input;
   std::getline(std::cin, input); 
-  if (input == "1" || input == "EST") {
+  if (input == "1" || input == "EST" || input == "est") {
     // Get the current time
     time_t now = time(0);
     tm *ltm = localtime(&now);
@@ -28,5 +32,15 @@ int main() {
 
     EST_Time(hour, minute, second);
   } 
+  else if (input == "2" || input == "UTC" || input == "utc") {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+
+    int hour = ltm->tm_hour;
+    int minute = ltm->tm_min;
+    int second = ltm->tm_sec;
+
+    UTC_Time(hour, minute, second);
+  }
   return 0;
 }
